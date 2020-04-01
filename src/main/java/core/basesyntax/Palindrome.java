dcfc65d94d20e59a1cbf71416c8277e677a283be
Nick97-git <nickarchangel97@gmail.com>
2020-04-01 11:44:22 +0300

    added solution

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        String cleanWord = text.toLowerCase().replaceAll("[^a-z0-9]","");
+        String reverseWord = new StringBuilder(cleanWord).reverse().toString();
+        return cleanWord.equals(reverseWord);
     }
 }
